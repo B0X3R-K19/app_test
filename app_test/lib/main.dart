11c49d0e76c9b842f31afa796f4b765189f3c1e2
B0X3R-19 <fabianparzer@gmx.net>
2023-11-15 09:05:16 +0100

    added new Features

--- app_test/lib/main.dart
+++ app_test/lib/main.dart
@@ -7,6 +7,15 @@ void main() {
   runApp(MyApp());
 }
 
+///
+///
+///
+/// If you read this... might think about adding a BMI calculator
+///
+///
+///
+///
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -59,13 +68,17 @@ class _MyHomePageState extends State<MyHomePage> {
     Widget page;
     switch (selectedIndex) {
       case 0:
-        page = TrainingTablePage();
+        page = StatisticsPage();
+        //page = FavoritesPage();
         break;
       case 1:
         page = GeneratorPage();
         break;
       case 2:
-        page = FavoritesPage();
+        page = TrainingTablePage();
+        break;
+      case 3:
+        page = ExcerciseSamplePage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -89,6 +102,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   NavigationRailDestination(
                       icon: Icon(Icons.calendar_today),
                       label: Text('Training')),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.track_changes),
+                    label: Text('Excercise Sample'),
+                  ),
                 ],
                 selectedIndex: selectedIndex,
                 onDestinationSelected: (value) {
@@ -109,6 +126,322 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       );
     });
+  }
+}
+
+class StatisticCard extends StatelessWidget {
+  final String title;
+  final String value;
+
+  StatisticCard({required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      margin: EdgeInsets.all(10),
+      child: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            Text(
+              title,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              value,
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StatisticsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Statistics'),
+      ),
+      body: Container(
+        color: Theme.of(context).colorScheme.primaryContainer,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              StatisticCard(
+                title: 'Wie oft warst du diese Woche schon beim Sport:',
+                value: '1.5 M',
+              ),
+              StatisticCard(
+                title: 'Welches ist deine Lieblings Muskelgruppe:',
+                value: 'Test',
+              ),
+              StatisticCard(
+                title: 'Das solltest du vielleicht öfters trainieren:',
+                value: '10 K',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ExcerciseSamplePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exercise Samples'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2, // Anzahl der Spalten
+        crossAxisSpacing: 16.0, // horizontaler Abstand zwischen den Elementen
+        mainAxisSpacing: 16.0, // vertikaler Abstand zwischen den Elementen
+        children: [
+          ImageCard(
+            imageUrl:
+                'https://www.fitundattraktiv.de/wp-content/uploads/2018/01/bankdruecken_muskelgruppen-schraegbankdruecken_mit_kurzhanteln.gif',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PageOne()),
+              );
+            },
+          ),
+          ImageCard(
+            imageUrl:
+                'https://www.fitundattraktiv.de/wp-content/uploads/2018/11/uebungen_latissimus-einarmiges_kurzhantelrudern.gif',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PageTwo()),
+              );
+            },
+          ),
+          ImageCard(
+            imageUrl:
+                'https://www.fitundattraktiv.de/wp-content/uploads/2018/02/45_grad_beinpresse.gif',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PageThree()),
+              );
+            },
+          ),
+          ImageCard(
+            imageUrl:
+                'https://www.fitundattraktiv.de/wp-content/uploads/2018/10/bizeps_uebungen_ohne_hilfsmittel-bizepscurls_einarmig.gif',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PageFour()),
+              );
+            },
+          ),
+          ImageCard(
+            imageUrl:
+                'https://www.fitundattraktiv.de/wp-content/uploads/2021/03/plank_uebung-mit_gewicht.gif',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PageFive()),
+              );
+            },
+          ),
+          ImageCard(
+            imageUrl:
+                'https://www.fitundattraktiv.de/wp-content/uploads/2021/03/seitheben_kurzhantel-stehend.gif',
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PageSix()),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ImageCard extends StatelessWidget {
+  const ImageCard({
+    required this.imageUrl,
+    required this.onTap,
+  });
+
+  final String imageUrl;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PageOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page One'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: DataTable(
+            columns: const <DataColumn>[
+              DataColumn(
+                label: Text('Übungstyp'),
+              ),
+              DataColumn(
+                label: Text('Sätze'),
+              ),
+              DataColumn(
+                label: Text('Wiederholungen'),
+              ),
+            ],
+            rows: const <DataRow>[
+              DataRow(
+                cells: <DataCell>[
+                  DataCell(Text('Bankdrücken')),
+                  DataCell(Text('4')),
+                  DataCell(Text('8-12')),
+                ],
+              ),
+              DataRow(
+                cells: <DataCell>[
+                  DataCell(Text('Butterfly')),
+                  DataCell(Text('4')),
+                  DataCell(Text('8-15')),
+                ],
+              ),
+              DataRow(
+                cells: <DataCell>[
+                  DataCell(Text('incline bench press')),
+                  DataCell(Text('4')),
+                  DataCell(Text('8-12')),
+                ],
+              ),
+              // ... (add more DataRow widgets as needed)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*
+class PageOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page One'),
+      ),
+      body: Center(
+        child: Text('Content of Page One'),
+      ),
+    );
+  }
+}
+*/
+class PageTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Two'),
+      ),
+      body: Center(
+        child: Text('Content of Page Two'),
+      ),
+    );
+  }
+}
+
+class PageThree extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Three'),
+      ),
+      body: Center(
+        child: Text('Content of Page Three'),
+      ),
+    );
+  }
+}
+
+class PageFour extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Four'),
+      ),
+      body: Center(
+        child: Text('Content of Page Four'),
+      ),
+    );
+  }
+}
+
+class PageFive extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Five'),
+      ),
+      body: Center(
+        child: Text('Content of Page Five'),
+      ),
+    );
+  }
+}
+
+class PageSix extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Six'),
+      ),
+      body: Center(
+        child: Text('Content of Page Six'),
+      ),
+    );
   }
 }
 
@@ -313,7 +646,6 @@ class FavoritesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-    ////
     if (appState.favorites.isEmpty) {
       return Center(
         child: Text('No favorites yet.'),
